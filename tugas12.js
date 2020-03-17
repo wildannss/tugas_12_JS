b7257f9d1ac7298b4b5301e302c1fecc4781662d
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:20:52 +0700

    tugas niomic

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function tugas12(){
+    let buah = [ 'Pisang', 'Jeruk' , 'Apel' , 'Mangga' ];
+    console.log(buah);
+    buah.pop();
+    console.log(buah);
+    buah.shift();
+    console.log(buah);
+};
+
+tugas12();
